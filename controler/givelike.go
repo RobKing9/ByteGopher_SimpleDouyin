53281a708165230b565eaa97e3a389b6e6f50803
yihao-bit <70103746+yihao-bit@users.noreply.github.com>
2022-06-05 22:21:45 +0800

    Add files via upload
    fix

---
+++ controler/givelike.go
@@ -0,0 +1,70 @@
+package controler
+
+import (
+	"ByteGopher_SimpleDouyin/model"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type likeResponse struct {
+	StatusCode int32  `json:"status_code"`
+	StatusMsg  string `json:"status_msg,omitempty"`
+}
+
+type GetFavouriteListResponse struct {
+	StatusCode int32         `json:"status_code"`
+	StatusMsg  string        `json:"status_msg,omitempty"`
+	VideoList  []model.Video `json:"video_list,omitempty"`
+}
+
+// FavoriteAction 点赞或者取消赞操作;
+func FavoriteAction(c *gin.Context) {
+	strUserId := c.GetString("userId")
+	userId, _ := strconv.ParseInt(strUserId, 10, 64)
+	strVideoId := c.Query("video_id")
+	videoId, _ := strconv.ParseInt(strVideoId, 10, 64)
+	strActionType := c.Query("action_type")
+	actionType, _ := strconv.ParseInt(strActionType, 10, 64)
+	like := new(model.LikeServiceImpl)
+	//获取点赞或者取消赞操作的错误信息
+	err := like.FavouriteAction(userId, videoId, int32(actionType))
+	if err == nil {
+		log.Printf("方法like.FavouriteAction(userid, videoId, int32(actiontype) 成功")
+		c.JSON(http.StatusOK, likeResponse{
+			StatusCode: 0,
+			StatusMsg:  "favourite action success",
+		})
+	} else {
+		log.Printf("方法like.FavouriteAction(userid, videoId, int32(actiontype) 失败：%v", err)
+		c.JSON(http.StatusOK, likeResponse{
+			StatusCode: 1,
+			StatusMsg:  "favourite action fail",
+		})
+	}
+}
+
+// GetFavouriteList 获取点赞列表;
+func GetFavouriteList(c *gin.Context) {
+	strUserId := c.Query("user_id")
+	strCurId := c.GetString("userId")
+	userId, _ := strconv.ParseInt(strUserId, 10, 64)
+	curId, _ := strconv.ParseInt(strCurId, 10, 64)
+	like := GetVideo()
+	videos, err := like.GetFavouriteList(userId, curId)
+	if err == nil {
+		log.Printf("方法like.GetFavouriteList(userid) 成功")
+		c.JSON(http.StatusOK, GetFavouriteListResponse{
+			StatusCode: 0,
+			StatusMsg:  "get favouriteList success",
+			VideoList:  videos,
+		})
+	} else {
+		log.Printf("方法like.GetFavouriteList(userid) 失败：%v", err)
+		c.JSON(http.StatusOK, GetFavouriteListResponse{
+			StatusCode: 1,
+			StatusMsg:  "get favouriteList fail ",
+		})
+	}
+}
